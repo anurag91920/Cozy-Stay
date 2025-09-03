8809fcc8aa0f1b435ddb5e346bbccc8713ec9976
Anurag Chaurasiya <anurag91920@gmail.com>
2025-09-03 15:43:14 +0530

    modified:   public/css/style.css 	modified:   routes/listing.js 	modified:   views/includes/footer.ejs 	modified:   views/includes/navbar.ejs 	new file:   views/listings/searchResults.ejs

--- routes/listing.js
+++ routes/listing.js
@@ -14,6 +14,30 @@ router.route("/")
 .get(wrapAsync(listingController.index))
 .post(isLoggedIn, upload.single('listing[image]'), validateListing, wrapAsync(listingController.createListing));
 
+//  Search Route should be before /:id
+router.get("/search", async (req, res, next) => {
+  try {
+    const query = req.query.q;
+    if (!query) {
+      req.flash("error", "Please enter a search term!");
+      return res.redirect("/listings");
+    }
+
+    const listings = await Listing.find({
+      $or: [
+        { title: { $regex: query, $options: "i" } },
+        { location: { $regex: query, $options: "i" } }
+      ]
+    });
+
+    // 🔹 Yahi pe render karo searchResults.ejs
+    res.render("listings/searchResults", { listings, query });
+    
+  } catch (err) {
+    next(err);
+  }
+});
+
 //New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
